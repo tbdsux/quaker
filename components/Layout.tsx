5bdbffbc8705d92790e3ad06b3edf42d085078bc
TheBoringDude <iamcoderx@gmail.com>
2021-01-21 18:50:37 +0800

    Init

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import Head from 'next/head'
+
+type LayoutProps = {
+  children?: ReactNode
+  title?: string
+}
+
+const Layout = ({ children, title }: LayoutProps) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <main className="antialiased">{children}</main>
+    </div>
+  )
+}
+
+export default Layout
