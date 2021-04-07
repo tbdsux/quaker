c1ae08b5940714c882b204479a74f33e51027494
TheBoringDude <iamcoderx@gmail.com>
2021-04-07 10:25:02 +0800

    use:: NextApiHandler as type

--- utils/middleware/method-handler.ts
+++ utils/middleware/method-handler.ts
@@ -1,19 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
 // methodHandler simple middleware that handles method not allowed responses
-const methodHandler = (
-  handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>,
-  allowedMethods: string[],
-) => (req: NextApiRequest, res: NextApiResponse) => {
-  if (!allowedMethods.includes(req.method)) {
-    // if the client's request method is not in
-    // the set `allowedMethods`, return error 405
-    res.setHeader('Allow', allowedMethods)
-    return res.status(405).end(`Method ${req.method} Not Allowed`)
-  }
+const methodHandler = (handler: NextApiHandler, allowedMethods: string[]) => (
+	req: NextApiRequest,
+	res: NextApiResponse
+) => {
+	if (!allowedMethods.includes(req.method)) {
+		// if the client's request method is not in
+		// the set `allowedMethods`, return error 405
+		res.setHeader("Allow", allowedMethods);
+		return res.status(405).end(`Method ${req.method} Not Allowed`);
+	}
 
-  // execute api function handler
-  return handler(req, res)
-}
+	// execute api function handler
+	return handler(req, res);
+};
 
-export default methodHandler
+export default methodHandler;
--- utils/middleware/session-handler.ts
+++ utils/middleware/session-handler.ts
@@ -1,17 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getLoginSession } from '@lib/auth'
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { getLoginSession } from "@lib/auth";
 
 // sessionHandler => handles user cookie (if exists, user is logged in; otherwise, returns error 403)
-const sessionHandler = (
-  handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>,
-) => async (req: NextApiRequest, res: NextApiResponse) => {
-  const session = await getLoginSession(req)
+const sessionHandler = (handler: NextApiHandler) => async (
+	req: NextApiRequest,
+	res: NextApiResponse
+) => {
+	const session = await getLoginSession(req);
 
-  if (!session) {
-    return res.status(403).json({})
-  }
+	if (!session) {
+		return res.status(403).json({});
+	}
 
-  return handler(req, res)
-}
+	return handler(req, res);
+};
 
-export default sessionHandler
+export default sessionHandler;
